f2e24f6fbbc08332a86847bc7b8e5d7d77e2bfcd
Matt Lane <matthewlane412@yahoo.com>
2021-09-23 15:01:08 -0500

    7.3.1

--- schema.sql
+++ schema.sql
@@ -36,9 +36,10 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+DROP TABLE dept_emp;
 CREATE TABLE dept_emp (
-  dept_no varchar(4) NOT NULL,
   emp_no INT NOT NULL,
+  dept_no varchar(4) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
@@ -46,14 +47,22 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 PRIMARY KEY (emp_no, dept_no)
 );
 
+
+DROP TABLE titles;
 CREATE TABLE titles (
   emp_no INT NOT NULL,
   title varchar NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 
+
 SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
